d09abc7cf89ce53ecf93e6e3339ef230550b7b46
abdullahtekunity <abdullahtekunity@gmail.com>
2026-02-13 04:17:46 +0500

    Initial commit

---
+++ app/data/social.ts
@@ -0,0 +1,48 @@
+import {
+  BiLinkExternal,
+  BiLogoCodepen,
+  BiLogoDribbble,
+  BiLogoGithub,
+  BiLogoGitlab,
+  BiLogoInstagram,
+  BiLogoLinkedinSquare,
+  BiLogoStackOverflow,
+  BiLogoSteam,
+  BiLogoUnsplash,
+  BiLogoYoutube,
+} from "react-icons/bi";
+import { FaFreeCodeCamp, FaProductHunt } from "react-icons/fa";
+import { SiCodewars } from "react-icons/si";
+import { FaHashnode, FaHashtag, FaSquareXTwitter } from "react-icons/fa6";
+
+export const socialLinks = [
+  {
+    id: 1,
+    name: "GitHub",
+    url: "https://github.com/AbdullahBapra",
+    icon: BiLogoGithub,
+    status: "social",
+  },
+  {
+    id: 2,
+    name: "X",
+    url: "https://x.com/AbdullahFa69598",
+    icon: FaSquareXTwitter,
+    status: "social",
+  },
+  {
+    id: 3,
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/abdullah-faheem-724196315/",
+    icon: BiLogoLinkedinSquare,
+    status: "social",
+  },
+
+  {
+    id: 6,
+    name: "Instagram",
+    url: "https://www.instagram.com/abdullahfaheem.ai?igsh=MW91am85OTBoMTQzNg==",
+    icon: BiLogoInstagram,
+    status: "social",
+  },
+];
