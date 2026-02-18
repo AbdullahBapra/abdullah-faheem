9906ebfad798d490b43c62bd888e803387133d81
abdullahtekunity <abdullahtekunity@gmail.com>
2026-02-18 22:52:04 +0500

    some change of sitemap

--- app/api/sitemap.xml/route.ts
+++ app/api/sitemap.xml/route.ts
@@ -1,36 +1,24 @@
-// app/api/sitemap.xml.ts
 import { NextResponse } from "next/server";
 
 export async function GET() {
   const baseUrl = "https://abdullah-faheem.vercel.app";
-
-  const staticPages = [
-    "",
-    "about",
-    "projects",
-    "blog",
-    "contact"
-  ];
+  const staticPages = ["", "about", "projects", "blog", "contact"];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${staticPages
-    .map((page) => {
-      return `
-    <url>
-      <loc>${baseUrl}/${page}</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.8</priority>
-    </url>
-  `;
-    })
-    .join("")}
+${staticPages
+  .map(
+    (page) => `
+  <url>
+    <loc>${baseUrl}/${page}${page ? "/" : ""}</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>`
+  )
+  .join("")}
 </urlset>`;
 
   return new NextResponse(sitemap, {
-    headers: {
-      "Content-Type": "application/xml",
-    },
+    headers: { "Content-Type": "application/xml" },
   });
 }
-
