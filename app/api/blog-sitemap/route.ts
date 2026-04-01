02668c0065f2cdcf37e22a5112b90ad0b1ccd289
AbdullahBapra <abdullahfaheem748@gmail.com>
2026-04-01 20:38:43 +0500

    Update

---
+++ app/api/blog-sitemap/route.ts
@@ -0,0 +1,31 @@
+import { postsQuery } from "@/lib/sanity.query";
+import { sanityFetch } from "@/lib/sanity.client";
+
+export async function GET() {
+  const posts = await sanityFetch({
+    query: postsQuery,
+    tags: ["Post"],
+  });
+
+  const filteredPosts = posts.filter(post => post.isPublished === true);
+
+  const urls = filteredPosts.map(post => `
+    <url>
+      <loc>https://abdullah-faheem.vercel.app/blog/${post.slug}</loc>
+      <lastmod>${post._updatedAt || post._createdAt}</lastmod>
+      <changefreq>weekly</changefreq>
+      <priority>0.9</priority>
+    </url>
+  `).join("");
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls}
+  </urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
